b840e480c9462e330e8340a2ab87efd285ec36e3
madhavsharma2106 <madhavsharma2106@gmail.com>
2020-04-27 02:24:23 +0530

    Added explanations

--- algorithms/breadcrumbs.js
+++ algorithms/breadcrumbs.js
@@ -1,3 +1,10 @@
+/**
+ * The Idea of the breadcrumbs technique is to keep track of values that we have already computed and refer to them if we ever encounter them again. This
+ *
+ * This Technique is better as looking for values in an object is O(1) and, therefore, we save on a lot of computation.
+ *
+ */
+
 //Checks if all elements in an array are unique.
 function isUnique(arr) {
   const breadcrumbs = {};
--- algorithms/memoization.js
+++ algorithms/memoization.js
@@ -2,6 +2,26 @@
  * Generic Memoization solution
  */
 
+const memoize = (cb) => {
+  const cache = {};
+  return (n) => {
+    if (n in cache) {
+      console.log("Fetching from cache", n);
+      return cache[n];
+    } else {
+      console.log("Calculating result");
+      let result = cb(n);
+      cache[n] = result;
+      return result;
+    }
+  };
+};
+
+// This is the function that does the calculation
+const times10 = (n) => n * 10;
+
+const multiplyBy10 = memoize(times10);
+
 /* Specific Solution */
 
 // const memoizedClosureTimes10 = () => {
@@ -24,9 +44,9 @@
 // // We are memoizing the value in the cache variable in the closure.
 // const multiplyBy10 = memoizedClosureTimes10();
 
-// console.log(multiplyBy10(4));
-// console.log(multiplyBy10(4));
-// console.log(multiplyBy10(4));
-// console.log(multiplyBy10(5));
-// console.log(multiplyBy10(6));
-// console.log(multiplyBy10(5));
+console.log(multiplyBy10(4));
+console.log(multiplyBy10(4));
+console.log(multiplyBy10(4));
+console.log(multiplyBy10(5));
+console.log(multiplyBy10(6));
+console.log(multiplyBy10(5));
